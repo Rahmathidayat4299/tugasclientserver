778dd21963078173d92d9acf1dab37de6a1d90dc
Rahmathidayat4299 <hrahmat950@gmail.com>
2021-12-10 13:45:36 +0700

    penyempurnaan fitur

--- cek_login.php
+++ cek_login.php
@@ -9,16 +9,28 @@ include "koneksi.php";
 			$pass	= isset($_POST['pass']) ? $_POST['pass'] : "";
 			$qry	= mysqli_query($db,"SELECT * FROM login WHERE username = '$user' AND password = '$pass'");
 			$sesi	= mysqli_num_rows($qry);
-            echo "<script>alert('Anda berhasil Log In');</script>";
-			echo "<meta http-equiv='refresh' content='0; url=index.php?user=$sesi'>";
-            
-          
-		
-		
-	}else{
-        echo "<meta http-equiv='refresh' content='0; url=login.php'>";
-        echo "<script>alert('Anda Gagal Log In');</script>";
-        header("location:index.php?pesan=gagal");
-    }
 
+			if ($sesi == 1)
+			{
+				$data_admin	= mysqli_fetch_array($qry);
+				$_SESSION['id_admin'] = $data_admin['id_admin'];
+				$_SESSION['sesi'] = $data_admin['nm_admin'];
+				
+				echo "<script>alert('Anda berhasil Log In');</script>";
+                header("Location:index.php");
+				
+				
+			}
+			else
+			{
+				echo "<meta http-equiv='refresh' content='0; url=login.php'>";
+				echo "<script>alert('Anda Gagal Log In');</script>";
+			}
+		
+		
+	}
+	else
+	{
+	  include "login.php";
+	}
 ?>
--- login.php
+++ login.php
@@ -13,8 +13,10 @@
         <div class="container">
           <h1>Login</h1>
             <form method="post" action="cek_login.php">
-                <label>Username</label><br>
-                <input type="text" name="user"><br>
+                <label>Username</label>
+                <br>
+                <input type="text" name="user">
+                <br>
                 <label>Password</label><br>
                 <input type="password" name="pass"><br>
                 <button type="submit"  name="submit">Log in</button>				
